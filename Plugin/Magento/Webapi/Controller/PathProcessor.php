2fe96da47fe04fcc552d7c936fcd5c938aa6faa5
espressobytes <tobias.bungers@gmail.com>
2020-02-21 16:04:10 +0100

    add logging of request headers

--- Plugin/Magento/Webapi/Controller/PathProcessor.php
+++ Plugin/Magento/Webapi/Controller/PathProcessor.php
@@ -6,6 +6,8 @@
 
 namespace Flancer32\LogApi\Plugin\Magento\Webapi\Controller;
 
+use Flancer32\LogApi\Helper\Config;
+
 /**
  * Log API paths processing.
  *
@@ -13,12 +15,24 @@ namespace Flancer32\LogApi\Plugin\Magento\Webapi\Controller;
  */
 class PathProcessor
 {
+
+    /** @var \Magento\Framework\App\RequestInterface */
+    private $request;
+
+    /** @var Config */
+    private $config;
+
     /** @var \Psr\Log\LoggerInterface */
     private $logger;
 
     public function __construct(
+        \Magento\Framework\App\RequestInterface $request,
+        \Flancer32\LogApi\Helper\Config $config,
         \Flancer32\LogApi\App\Logger $logger
-    ) {
+    )
+    {
+        $this->request = $request;
+        $this->config = $config;
         $this->logger = $logger;
     }
 
@@ -34,11 +48,36 @@ class PathProcessor
         \Magento\Webapi\Controller\PathProcessor $subject,
         callable $proceed,
         $pathInfo
-    ) {
+    )
+    {
         $result = $proceed($pathInfo);
-        $msg = "Route: '$pathInfo' => '$result'";
-        $this->logger->info($msg);
+
+        try {
+
+            // get method and route
+            $method = $this->request->getMethod();
+            $msg = "Route ($method): '$pathInfo' => '$result'";
+
+            $this->logger->info($msg);
+
+            // log header (if setup in the config)
+            if ($this->config->getLogHeaderValues()) {
+                $this->logHeaders();
+            }
+
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+            $this->logger->error($e->getTraceAsString());
+        }
+
         return $result;
+    }
+
+    private function logHeaders()
+    {
+        $headersAsString = $this->request->getHeaders()->toString();
+        $msgHeaders = "Request-Headers: {$headersAsString}";
+        $this->logger->info($msgHeaders);
     }
 
 }
